0e79337d15136b3123cdf437b9b6390970a2724e
Szymon Kedra <s5zee@wp.pl>
2023-05-04 21:14:39 +0200

    dishTypes array added

---
+++ src/Components/DishForm/FormField/dishTypes.ts
@@ -0,0 +1,18 @@
+export const dishTypes = [
+  {
+    value: "",
+    tag: "Select Type",
+  },
+  {
+    value: "pizza",
+    tag: "Pizza",
+  },
+  {
+    value: "soup",
+    tag: "Soup",
+  },
+  {
+    value: "sandwich",
+    tag: "Sandwich",
+  },
+];
